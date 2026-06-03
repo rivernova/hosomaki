df17b37d40dc7218694f531858131b4644e74855
rivernona <brivero30@gmail.com>
2026-06-03 18:34:29 +0200

    HMKI Spinner renders with pale lavender

--- internal/spinner/spinner.go
+++ internal/spinner/spinner.go
@@ -11,54 +11,83 @@ import (
 	"time"
 )
 
-// spinner is a simple terminal spinner that runs in a separate goroutine
+// simple terminal spinner implementation with support for dynamic labels and RGB colors
 
 var frames = []string{"⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"}
 
+const (
+	clearLine      = "\r\033[K"
+	resetColor     = "\x1b[0m"
+	tickerDuration = 80 * time.Millisecond
+)
+
 type Spinner struct {
-	label chan string
-	stop  chan struct{}
-	done  chan struct{}
-	once  sync.Once
+	mu          sync.RWMutex
+	label       string
+	colorEscSeq string
+	stop        chan struct{}
+	done        chan struct{}
+	once        sync.Once
 }
 
 func Start(label string) *Spinner {
 	s := &Spinner{
-		label: make(chan string, 1),
+		label: label,
 		stop:  make(chan struct{}),
 		done:  make(chan struct{}),
 	}
-	go func() {
-		defer close(s.done)
-		current := label
-		i := 0
-		for {
-			select {
-			case <-s.stop:
-				fmt.Fprintf(os.Stderr, "\r\033[K")
-				return
-			case l := <-s.label:
-				current = l
-			default:
-				fmt.Fprintf(os.Stderr, "\r%s %s", frames[i%len(frames)], current)
-				i++
-				time.Sleep(80 * time.Millisecond)
-			}
-		}
-	}()
+
+	go s.run()
 	return s
 }
 
-func (s *Spinner) SetLabel(label string) {
-	select {
-	case s.label <- label:
-	default:
+func StartWithRGB(label string, r, g, b int) *Spinner {
+	s := Start(label)
+	s.SetRGB(r, g, b)
+	return s
+}
+
+func (s *Spinner) run() {
+	defer close(s.done)
+
+	ticker := time.NewTicker(tickerDuration)
+	defer ticker.Stop()
+
+	var frameIdx int
+
+	for {
 		select {
-		case <-s.label:
-		default:
+		case <-s.stop:
+			fmt.Fprint(os.Stderr, clearLine)
+			return
+		case <-ticker.C:
+			s.mu.RLock()
+			currentLabel := s.label
+			currentColor := s.colorEscSeq
+			s.mu.RUnlock()
+
+			frame := frames[frameIdx%len(frames)]
+			frameIdx++
+
+			if currentColor == "" {
+				fmt.Fprintf(os.Stderr, "\r%s %s", frame, currentLabel)
+			} else {
+				fmt.Fprintf(os.Stderr, "\r%s%s %s%s", currentColor, frame, currentLabel, resetColor)
+			}
 		}
-		s.label <- label
 	}
+}
+
+func (s *Spinner) SetLabel(label string) {
+	s.mu.Lock()
+	s.label = label
+	s.mu.Unlock()
+}
+
+func (s *Spinner) SetRGB(r, g, b int) {
+	s.mu.Lock()
+	s.colorEscSeq = fmt.Sprintf("\x1b[38;2;%d;%d;%dm", r, g, b)
+	s.mu.Unlock()
 }
 
 func (s *Spinner) Stop() {
